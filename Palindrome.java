d7ebb3bdf287d7064e5d4dd768ae8877b138cc7f
khananam17 <144773408+khananam17@users.noreply.github.com>
2023-09-13 13:14:56 +0530

    Add files via upload
    loop concept in java

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+public class Palindrome{
+public static void main(String args[]){
+int n = 1234;
+int num=n;
+int reverse=0;
+while(n>0){
+int remainder = n%10;
+reverse=(reverse*10)+remainder;
+n=n/10;
+
+}
+if (num ==reverse){
+ System.out.println(num + "is a  Palindrome number");
+}else{
+System.out.println(num + "is a not Palindrome number");
+}
+}
+}
